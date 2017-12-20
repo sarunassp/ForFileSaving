f91570e0cf1d086fd8ff24951b9652570cda65cf
sarunassp <sarunassp@gmail.com>
2017-12-20 14:42:20 +0200

    asdasdasd

--- psp/3 uzduotis/TreciaUzduotis/Decorator/Program.cs
+++ psp/3 uzduotis/TreciaUzduotis/Decorator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Decorator.Components;
 using Decorator.Decorators;
 
@@ -9,8 +10,8 @@ namespace Decorator
         public static void Main (string[] args)
         {
             AbstractHouse asd = new WindowDecorator (new HouseResizeDecorator (new House (), "resize"), "window");
-
-            asd.DeleteByRole ("resize");
+            
+            Helpers.DeleteByRole (ref asd, "resize");
 
             Console.WriteLine (asd.GetDescription ());
         }
@@ -18,7 +19,7 @@ namespace Decorator
 
     public static class Helpers
     {
-        public static void DeleteByRole (this AbstractHouse source, string role)
+        public static void DeleteByRole (ref AbstractHouse source, string role)
         {
             var asd = source as HouseDecorator;
             if (null == asd)
@@ -26,8 +27,19 @@ namespace Decorator
 
             if (asd.MRole == role)
                 source = asd.MHouse;
-            
-            DeleteByRole (asd.MHouse, role);
+            else
+                Helpers.DeleteByRole (ref asd.MHouse, role);
+        }
+
+        public static HouseDecorator GetByRole (AbstractHouse source, string role)
+        {
+            var asd = source as HouseDecorator;
+            if (null == asd)
+                throw new ArgumentOutOfRangeException (role);
+
+            if (role == asd.MRole)
+                return asd;
+            return GetByRole (asd.MHouse, role);
         }
     }
 }
